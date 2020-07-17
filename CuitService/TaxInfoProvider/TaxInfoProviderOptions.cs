65d3c7fc386fd8c102e4a0ec0a1ad00b476c3c53
Andrés Moschini <andresmoschini@gmail.com>
2020-07-17 13:01:24 -0300

    feat: allow to use dummy data in place of doing a real request

--- CuitService/TaxInfoProvider/TaxInfoProviderOptions.cs
+++ CuitService/TaxInfoProvider/TaxInfoProviderOptions.cs
@@ -2,6 +2,7 @@ namespace CuitService.TaxInfoProvider
 {
     public class TaxInfoProviderOptions
     {
+        public bool UseDummyData { get; set; }
         public string UriTemplate { get; set; } = string.Empty;
         public string Host { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
--- CuitService/TaxInfoProvider/TaxInfoProviderServiceCollectionExtensions.cs
+++ CuitService/TaxInfoProvider/TaxInfoProviderServiceCollectionExtensions.cs
@@ -9,7 +9,16 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             services.ConfigureOptions<ConfigureTaxInfoProviderOptions>();
 
-            services.AddTransient<ITaxInfoProviderService, TaxInfoProviderService>();
+            services.AddSingleton<DummyTaxInfoProviderService>();
+            services.AddTransient<TaxInfoProviderService>();
+
+            services.AddTransient(serviceProvider =>
+            {
+                var taxInfoProviderOptions = serviceProvider.GetRequiredService<IOptions<TaxInfoProviderOptions>>();
+                return taxInfoProviderOptions.Value.UseDummyData
+                    ? (ITaxInfoProviderService)serviceProvider.GetRequiredService<DummyTaxInfoProviderService>()
+                    : serviceProvider.GetRequiredService<TaxInfoProviderService>();
+            });
 
             return services;
         }
